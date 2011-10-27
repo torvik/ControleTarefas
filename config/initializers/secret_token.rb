d653ea773cd083801403b036066ceed1f6fb54a3
Victor <pless.victor@gmail.com>
2011-10-27 20:16:05 -0200

    First commit app Tasklist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasklist::Application.config.secret_token = '5fc3436473259392adfa453bfc77cfc3718ea0910b6baf05c368dce8fa8f72c50fb82ba32574fbd4d2f85ac3e085c875e79088a9d84fdb3ff69a33f3034a712a'
